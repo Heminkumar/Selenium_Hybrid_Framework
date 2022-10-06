77706fe66db46b8c66e6458dc16f8fc5290f5a58
Heminkumar Patel <heminswa67@gmail.com>
2022-10-06 15:41:55 +0530

    Parallel execution with -n=3 (num of TCs)

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -3,7 +3,21 @@ from selenium import webdriver
 import unittest
 
 @pytest.fixture()
-def web_setup():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+def web_setup(browser):
+    if browser == 'chrome':
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+    elif browser == 'firefox':
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+    else:
+        driver = webdriver.Chrome() # if not pass any browser then run in chrome default browser, webdriver.Ie()
+        driver.maximize_window()
     return driver
+
+def pytest_addoption(parser): # This will get the value from CLI/Hooks
+    parser.addoption("--browser")
+
+@pytest.fixture()
+def browser(request): # This will return the browser value to web_setup method
+    return request.config.getoption("--browser")
--- utilities/customLogger.py
+++ utilities/customLogger.py
@@ -7,7 +7,10 @@ class LogGenerator:
     def loggen():
         logger = logging.getLogger(__name__)
         dir = os.path.join(os.getcwd(),"Logs/"+datetime.now().strftime('%d_%m_%Y-%H_%M'))
-        dir1 = os.mkdir(dir)
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+        else:
+            print("Log Directory is already exists....")
         fileHandler = logging.FileHandler(str(dir) +'/' +"automation_info.log")
         logger.addHandler(fileHandler)
         formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
