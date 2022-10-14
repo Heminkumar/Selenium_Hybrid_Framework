d0963b25a9bc166e65ea1778dae8e5bc75efce57
Heminkumar Patel <heminswa67@gmail.com>
2022-10-14 18:03:55 +0530

    New commit on 14th Oct-6PM

--- pageObjects/AddcustomerPage.py
+++ pageObjects/AddcustomerPage.py
@@ -2,7 +2,6 @@
 import time
 from selenium.webdriver.support.ui import Select
 
-<<<<<<< HEAD
 class AddCustomer:
     lnkCustomers_menu_xpath = '//a[@href="#"]//p[contains(text(),"Customers")]'
     lnkCustomers_menuItem_xpath = ''
@@ -26,28 +25,3 @@ class AddCustomer:
 
     def __init__(self,driver):
         self.driver = driver
-=======
-class AddCustomer():
-    lnkCustomers_menu_xpath = '//a[@href="#"]//p[contains(text(),"Customers")]'
-    lnkCustomers_menuitem_xpath =
-    btnAddnew_xpath =
-    txtEmail_xpath =
-    txtPassword_xpath =
-    txtcustomerRoles_xpath =
-    lstitemAdministrators_xpath =
-    lstitemRegistered_xpath =
-    lstitemGuests_xpath =
-    lstitemVendors_xpath =
-    drpmgrOfVendor_xpath =
-    rdMaleGender_id =
-    rdFeMaleGender_id =
-    txtFirstName_xpath =
-    txtLastName_xpath =
-    txtDob_xpath =
-    txtCompanyName_xpath =
-    txtAdminContent_xpath =
-    btnSave_xpath =
-
-    def __init__(self,driver):
-        self.driver = driver
->>>>>>> origin/master
